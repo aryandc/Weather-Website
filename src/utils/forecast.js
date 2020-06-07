20ec7b31ae989fd0d4af0f4850671f2e78a69f6c
Aryan Choudhary <adchoudhary@dons.usfca.edu>
2020-06-07 03:27:24 -0700

    Added more information in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -16,12 +16,17 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       callback(
         undefined,
-        body.current.weather_descriptions[0] +
+        "Observed at " +
+          body.current.observation_time +
+          ": " +
+          body.current.weather_descriptions[0] +
           ". It is currently " +
           body.current.temperature +
           " degrees out. It feels like " +
           body.current.feelslike +
-          " degrees out."
+          " degrees out. The wind speed is " +
+          body.current.wind_speed +
+          " mph."
       );
     }
   });
